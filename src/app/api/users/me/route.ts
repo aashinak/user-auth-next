3c178d5bc0a35c53fc31e082ececfc8eb2383744
Aashin A K <aashin9605@gmail.com>
2024-06-20 23:16:10 +0530

    complete profile functionality

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,20 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import User from '@/models/userModel'
+import { NextRequest, NextResponse } from 'next/server'
+
+
+connect()
+
+export async function GET(req: NextRequest) {
+    try {
+        const userId = await getDataFromToken(req)
+        const user = await User.findById({ userId }).select("-password")
+        if (!user) {
+            return NextResponse.json({error: "User not found"},{status: 400})
+        }
+        return NextResponse.json({message: "User found", data: user},{status: 200})
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},{status: 500})
+    }
+}
